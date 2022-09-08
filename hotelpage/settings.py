47d2232d190d34fbcce12747e4b8e3b01229294d
Oscar <oscarblessed04@gmail.com>
2022-09-08 05:07:17 +0100

    added a few changes to environment variables

--- hotelpage/settings.py
+++ hotelpage/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cp&x&shcl_re2dd)szd(go=@-b+_isemm$s+^8umqppzbqfzvd'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuoscar.herokuapp.com']
 
 
 # Application definition
